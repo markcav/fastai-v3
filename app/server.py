4b99b3d948ce1e01d7b60c4c34790b4ac7e6b8d0
Mark Cavanaugh <emailmarkcav@gmail.com>
2019-09-30 15:29:24 -0700

    added export.pkl

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=13erPwaM2BST9i8jghE0ybYI2TOocKkTw'
 export_file_name = 'export.pkl'
 
 classes = ['cleanbedroom', 'dirtybedroom']
