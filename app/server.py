63d38a932e2bfa6a80bbe5ad718a83c3847c7b19
Mark Cavanaugh <emailmarkcav@gmail.com>
2019-09-30 22:40:26 -0700

    update downloadable link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=13erPwaM2BST9i8jghE0ybYI2TOocKkT'
+export_file_url = 'https://drive.google.com/uc?export=download&id=13erPwaM2BST9i8jghE0ybYI2TOocKkTw'
 export_file_name = 'export.pkl'
 
 classes = ['cleanbedroom', 'dirtybedroom']
